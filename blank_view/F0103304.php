e70e3061cf35dbcb10a897b197a8a555826c13a9
mmaaxx <2ovob4ehko@ukr.net>
2015-06-24 00:36:41 +0300

    Додав відображення одного з бланків

---
+++ blank_view/F0103304.php
@@ -0,0 +1,265 @@
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td  align="center" >
+      <h2>ПОДАТКОВА ДЕКЛАРАЦІЯ ПЛАТНИКА ЄДИНОГО ПОДАТКУ -  ФІЗИЧНОЇ ОСОБИ - ПІДПРИЄМЦЯ</h2>
+    </td>
+  </tr>
+</table>
+<table  style="margin:0 auto; width:500px;text-align:center;" border="1" cellpadding="0" cellspacing="0">
+  <tr>
+    <td>звітна</td>
+    <td>рік</td>
+    <td>2015</td>
+	</tr>
+</table>
+<table width="100%" border="1" cellpadding="0" cellspacing="0">
+  <tr>
+    <td colspan="2">Прізвище, ім'я, по батькові платника податку:</td>
+  </tr>
+  <tr>
+    <td colspan="2">
+		    <input type="text" style="width:100%;" name="HNAME" value="ФОП Петренко Петро Петрович">
+    </td>
+	</tr>
+  <tr>
+    <td>Реєстраційний номер облікової картки платника податків - фізичної особи - підприємця:</td>
+		<td>
+      <input type="text" style="width:100%;" name="HTIN" value="1234567890">
+    </td>
+	</tr>
+	<tr>
+    <td colspan="2" style="font-size:12px;"> або серія та номер паспорта (для фізичних осіб, які через релігійні переконання відмовляються від прийняття реєстраційного номера
+облікової картки платника податків та повідомили про це відповідний орган державної податкової служби і мають відмітку у паспорті)</td>
+	</tr>
+</table>
+<table width="100%" border="1" cellpadding="0" cellspacing="0">
+	<tr>
+    <td colspan="3">Податкова адреса (місце проживання) платника податку</td>
+		<td>поштовий індекс</td>
+		<td><input type="text" style="width:100%;" name="HZIP" value="18000"></td>
+  </tr>
+	<tr>
+		<td>(область, місто):</td>
+		<td><input type="text" style="width:100%;" name="HREG" value="Черкаська"></td>
+		<td><input type="text" style="width:100%;" name="HCITY" value="Черкаси"></td>
+		<td>міжміський код</td>
+		<td><input type="text" style="width:100%;" name="HINTURB" value="0472"></td>
+	</tr>
+	<tr>
+		<td>Адреса:</td>
+		<td colspan="2"><input type="text" style="width:100%;" name="HLOC" value="вул. Гагаріна 7 кв 1"></td>
+		<td>телефон</td>
+		<td><input type="text" style="width:100%;" name="HTEL" value="764862"></td>
+	</tr>
+	<tr>
+		<td>Електронна адреса:</td>
+		<td colspan="2"><input type="text" style="width:100%;" name="HEMAIL" value="1@ukr.net"></td>
+		<td>факс:</td>
+		<td><input type="text" style="width:100%;" name="HFAX" value="764862"></td>
+	</tr>
+</table>
+<table width="100%" border="1" cellpadding="0" cellspacing="0">
+  <tr>
+    <td>найменування органу державної податкової служби, до якого подається податкова декларація:</td>
+	</tr>
+  <tr>
+		<td><input type="text" style="width:100%;" name="HSTI" value="2301 ДПІ у м. Черкасах"></td>
+	</tr>
+</table>
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td>Види підприємницької діяльності, які здійснювалися у звітному періоді:</td>
+	</tr>
+</table>
+<table width="100%" border="1" cellpadding="0" cellspacing="0">
+  <tr>
+		<td>номер згідно з КВЕД</td>
+		<td>назва згідно з КВЕД</td>
+ </tr>
+ <tr>
+   <td>62.02</td>
+   <td>Програмування</td>
+ </tr>
+</table>
+<table width="100%" border="1" cellpadding="0" cellspacing="0">
+  <tr>
+    <td>Фактична чисельність найманих працівників у  звітному  періоді:</td>
+		<td><input type="text" style="width:100%;" name="HNACTL" value="12"></td>
+	</tr>
+</table><br>
+<table border="0"  width="100%" cellspacing="0" bordercolor="#000000">
+  <tr>
+    <td align="center"><b>ПОКАЗНИКИ ГОСПОДАРСЬКОЇ ДІЯЛЬНОСТІ ДЛЯ ПЛАТНИКІВ ЄДИНОГО ПОДАТКУ ІІІ  ГРУПИ </b></td>
+  </tr>
+</table>
+<table border="1" width="100%" cellspacing="0" bordercolor="#000000">
+  <tr align="center">
+    <td>Назва показника</td>
+    <td>Код рядка</td>
+    <td>Обсяг (грн.)</td>
+  </tr>
+  <tr align="center">
+    <td>1</td>
+    <td>2</td>
+    <td>3</td>
+  </tr>
+  <tr>
+    <td>Сума доходу за звітний (податковий) період, що оподатковується за ставкою 2 %</td>
+    <td align="center">11</td>
+    <td align="right"><input type="text" style="width:100%;" name="R011G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>Сума доходу за звітний (податковий) період, що оподатковується за ставкою 4 %</td>
+    <td align="center">12</td>
+    <td align="right"><input type="text" style="width:100%;" name="R012G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>Сума доходу, що перевищує обсяги, встановлені підпунктом 3 пункту 291.4 статті 291 глави 1 розділу XIV Податкового кодексу України, у звітному (податковому) періоді</td>
+    <td align="center">13</td>
+    <td align="right"><input type="text" style="width:100%;" name="R013G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>Сума доходу, отриманого при застосуванні іншого способу розрахунків, ніж передбачено пунктом 291.6 статті 291 глави 1 розділу XIV Податкового кодексу України, у звітному (податковому) періоді</td>
+    <td align="center">14</td>
+    <td align="right"><input type="text" style="width:100%;" name="R014G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>Сума доходу, отриманого від здійснення видів діяльності, які не дають права на застосування спрощеної системи оподаткування, у звітному (податковому) періоді</td>
+    <td align="center">15</td>
+    <td align="right"><input type="text" style="width:100%;" name="R015G3" value="12"></td>
+  </tr>
+</table><br>
+<table border="0"  width="100%" cellspacing="0" bordercolor="#000000">
+  <tr>
+    <td align="center"><b>ВИЗНАЧЕННЯ ПОДАТКОВИХ ЗОБОВ'ЯЗАНЬ ПО ЄДИНОМУ ПОДАТКУ</b></td>
+  </tr>
+</table>
+<table border="1" width="100%" cellspacing="0" bordercolor="#000000">
+  <tr align="center">
+    <td>Назва показника</td>
+    <td>Код рядка</td>
+    <td>Сума (грн.)</td>
+  </tr>
+  <tr align="center">
+    <td>1</td>
+    <td>2</td>
+    <td>3</td>
+  </tr>
+  <tr>
+    <td><b>Загальна сума доходу за звітний (податковий) період</b><br/>(сума значень рядків 01 + 02 + 03 + 04 + 05 + 06 + 07 + 08 + 09 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20)</td>
+    <td align="center">21</td>
+    <td align="right"><input type="text" style="width:100%;" name="R021G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>Сума податку за ставкою 15 % <br />((рядок 02 + рядок 03 + рядок 04 + рядок 05 + рядок 07 + рядок 08 + рядок 09 + рядок 10 + рядок 13 + рядок 14 + рядок 15 + рядок 18 + рядок 19 + рядок 20) х 15 %)</td>
+    <td align="center">22</td>
+    <td align="right"><input type="text" style="width:100%;" name="R022G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>Сума податку за ставкою 3 % (рядок 11 х 3 %)</td>
+    <td align="center">23</td>
+    <td align="right"><input type="text" style="width:100%;" name="R023G3" value="12"></td>
+  </tr>
+
+	 <tr>
+    <td>Сума податку за ставкою 5 % (рядок 12 х 5 %)</td>
+    <td align="center">24</td>
+    <td align="right"><input type="text" style="width:100%;" name="R024G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>Сума податку за ставкою 7 % (рядок 16 х 7 %)</td>
+    <td align="center">25</td>
+    <td align="right"><input type="text" style="width:100%;" name="R025G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>Сума податку за ставкою 10 % (рядок 17 х 10 %)</td>
+    <td align="center">26</td>
+    <td align="right"><input type="text" style="width:100%;" name="R026G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>Нараховано всього за звітний (податковий) період <br/>(рядок 22 + рядок 23 +  рядок 24 + рядок 25 + рядок 26))</td>
+    <td align="center">27</td>
+    <td align="right"><input type="text" style="width:100%;" name="R027G3" value="12"></td>
+  </tr>
+	<tr>
+    <td>Нараховано за попередній звітний (податковий) період (значення рядка 27 декларації попереднього звітного (податкового) періоду)</td>
+    <td align="center">28</td>
+    <td align="right"><input type="text" style="width:100%;" name="R028G3" value="12"></td>
+  </tr>
+	<tr>
+    <td>Сума єдиного податку, яка підлягає нарахуванню та сплаті в бюджет за підсумками поточного звітного (податкового) періоду (рядок 27 - рядок 28)</td>
+    <td align="center">29</td>
+    <td align="right"><input type="text" style="width:100%;" name="R029G3" value="12"></td>
+  </tr>
+</table><br/>
+<table border="0"  width="100%" cellspacing="0" bordercolor="#000000">
+  <tr>
+    <td align="center"><b>ВИЗНАЧЕННЯ ПОДАТКОВИХ ЗОБОВ'ЯЗАНЬ У ЗВ'ЯЗКУ З ВИПРАВЛЕННЯМ САМОСТІЙНО ВИЯВЛЕНИХ ПОМИЛОК*</b></td>
+  </tr>
+</table>
+<table border="1" width="100%" cellspacing="0" bordercolor="#000000">
+  <tr align="center">
+    <td>Назва показника</td>
+    <td>Код рядка</td>
+    <td>Сума (грн.)</td>
+  </tr>
+  <tr align="center">
+    <td>1</td>
+    <td>2</td>
+    <td>3</td>
+  </tr>
+  <tr>
+    <td>Сума єдиного податку, яка підлягала перерахуванню до бюджету, за даними звітного (податкового) періоду, в якому виявлена помилка (рядок 29 відповідної декларації)</td>
+    <td align="center">30</td>
+    <td align="right"><input type="text" style="width:100%;" name="R030G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>Уточнена сума податкових зобов'язань за звітний (податковий) період, у якому виявлена помилка</td>
+    <td align="center">31</td>
+    <td align="right"><input type="text" style="width:100%;" name="R031G3" value="12"></td>
+  </tr>
+  <tr>
+    <td colspan="3"><b>Розрахунки у зв'язку з виправленням помилки:</b></td>
+  </tr>
+	<tr>
+    <td>збільшення суми, яка підлягала перерахуванню до бюджету <br/>(рядок 31 - рядок 30, якщо рядок 31 &gt; рядка 30) </td>
+    <td align="center">32</td>
+    <td align="right"><input type="text" style="width:100%;" name="R032G3" value="12"></td>
+  </tr>
+  <tr>
+    <td>зменшення суми, яка підлягала перерахуванню до бюджету**<br/>(рядок 31- рядок 30, якщо рядок 31 &lt; рядка 30)</td>
+    <td align="center">33</td>
+    <td align="right"><input type="text" style="width:100%;" name="R033G3" value="12"></td>
+  </tr>
+	<tr>
+    <td>Сума штрафу, яка нарахована платником податку самостійно у зв'язку з виправленням помилки,
+      <input type="text" style="width:20px;" name="R034G1" value="12"> % (рядок 32 х 3 % або 5 %)</td>
+    <td align="center">34</td>
+    <td align="right"><input type="text" style="width:100%;" name="R034G3" value="12"></td>
+  </tr>
+	<tr>
+    <td>Сума пені, яка нарахована платником податку самостійно відповідно до підпункту 129.1.2 пункту 129.1 статті 129 глави 12 розділу II Податкового кодексу України</td>
+    <td align="center">35</td>
+    <td align="right"><input type="text" style="width:100%;" name="R035G3" value="12"></td>
+  </tr>
+</table>
+<table border="0"  width="100%" cellspacing="0" bordercolor="#000000">
+  <tr>
+    <td>*  Заповнюється  платником податку, який уточнює податкові зобов'язання.</td>
+  </tr>
+	<tr>
+    <td >** Відображаються тільки позитивні значення.</td>
+  </tr>
+</table><br/>
+<table width="100%" cellspacing="3" border="0">
+  <tr>
+    <td>Фізична особа - підприємець</td>
+    <td><input type="text" style="width:100%;" name="HBOS" value="12"></td>
+		<td><input type="text" style="width:100px;" name="HKBOS" value="12"></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td align="center"><font size="-1">(ініціали та прізвище)</font></td>
+    <td colspan="2">(реєстраційний номер облікової картки платника податків)</td>
+  </tr>
+</table>
